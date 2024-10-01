4343db054678d5cd6768b04a2eeeccfd1a0cd3ff
Felix Idén <ouc98oec@students.codeinstitute.net>
2024-10-01 08:37:54 +0000

    initial commit, add print welcome message

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+""" main function """ 
+def main():
+    """ Welcome message """ 
+    print('Welcome to your budget app')
+    initial_budget = input('Please enter your initial budget: ')
+    print(initial_budget)
+
+main()
