08982574bc59c41cb0ae8a3e6e6841c00fa5d828
Felix Idén <ouc98oec@students.codeinstitute.net>
2024-10-02 08:22:35 +0000

    cleaned up trailing whitespace

--- run.py
+++ run.py
@@ -1,34 +1,41 @@
 import json
+
+
 def add_expense(expenses, description, amount):
     expenses.append({'description': description, 'amount': amount})
     print(f'Added expense: {description} Amount: {amount}')
+
 
 def get_total_expenses(expenses):
     sum = 0
     for expense in expenses:
         sum += expense['amount']
-    return sum    
+    return sum
+
 
 def get_balance(budget, expenses):
     return budget - get_total_expenses(expenses)
+
 
 def show_budget_details(budget, expenses):
     print(f'Total Budget: {budget}')
     print('expenses:')
     for expense in expenses:
         print(f'- {expense['description']}: {expense['amount']}')
-        print(f'Total Spent: {get_total_expenses(expenses)}')
-        print(f'Remaining budget: {get_balance(budget, expenses)}')
+    print(f'Total Spent: {get_total_expenses(expenses)}')
+    print(f'Remaining budget: {get_balance(budget, expenses)}')
+
 
 def load_budget_data(filepath):
-    try: 
+    try:
         with open(filepath, 'r') as file:
             data = json.load(file)
             return data['initial_budget'], data['expenses']
 
-    except (FileNotFoundError, json.JSONDecodeError):
-        return 0, []        
-    """ If file not found, return default values, 0 and empty list """
+            except (FileNotFoundError, json.JSONDecodeError):
+                return 0, []
+            """ If file not found, return default values, 0 and empty list """
+
 
 def save_budget_details(filepath, initial_budget, expenses):
     data = {
@@ -39,21 +46,23 @@ def save_budget_details(filepath, initial_budget, expenses):
     with open(filepath, 'w') as file:
         json.dump(data, file, )
 
+
 def reset_expenses(expenses):
     expenses.clear()
     """ Reset all expenses to 0 """
 
+
 def main():
-    """ main function """      
+    """ main function """
     print('Welcome to your budget app')
     """ Welcome message """
-    
+
     filepath = '/workspaces/P3/budget_data.json'
     """ Defines the path to the json file """
     initial_budget, expenses = load_budget_data(filepath)
     budget = initial_budget
-    
-    while True: 
+
+    while True:
         """ While loop to run until we manually break out """
         print('\nWhat would you like to do?')
         print('\n1. Add an expense')
@@ -66,39 +75,40 @@ def main():
         if choice == '1':
             description = input('Enter expense description: ')
             amount = float(input('Enter expense amount: '))
-            """ Changes input from string to float """ 
+            """ Changes input from string to float """
             add_expense(expenses, description, amount)
 
         elif choice == '2':
             show_budget_details(budget, expenses)
             """ Shows budget details """
-            
+
         elif choice == '3':
-            reset_expenses(expenses)  
-            """ Resets all expenses""" 
+            reset_expenses(expenses)
+            """ Resets all expenses"""
             initial_budget = float(input('Enter your initial budget: '))
-            budget = initial_budget  
+            budget = initial_budget
             """ Update the budget variable to reflect the new input """
-            save_budget_details(filepath, initial_budget, expenses)  
+            save_budget_details(filepath, initial_budget, expenses)
             """ Save the updated budget and expenses """
 
         elif choice == '4':
             additional_amount = float(input('Enter amount to add to budget: '))
-            budget += additional_amount   
+            budget += additional_amount
             """ Add more to the current budget"""
-            initial_budget += additional_amount   
+            initial_budget += additional_amount
             """ Update the initial budget """
-            save_budget_details(filepath, initial_budget, expenses)  
+            save_budget_details(filepath, initial_budget, expenses)
             """ Save the updated budget """
-            print(f'Added {additional_amount} to the budget. New budget: {budget}')
-    
-            
+            print(f'Added {additional_amount} to budget. New budget: {budget}')
+
         elif choice == '5':
             save_budget_details(filepath, initial_budget, expenses)
             print('Closing budget app, see you soon')
             break
             """ Closing the app """
-        else: 
+        else:
             print('Invalid choice, please try again')
             """ if input is something else then 1,2,3,4,5 """
+
+
 main()
